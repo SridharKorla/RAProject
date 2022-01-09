14c787a35a92dc4c94d7dc87e78c1d3161af07f9
VEERAVENKATA KORLA <VEERAVENKATA.KORLA@LibertyMutual.com>
2022-01-08 20:27:47 -0800

    Selenium test commit 8:37

--- SeleniumTests/src/test/java/TC1.java
+++ SeleniumTests/src/test/java/TC1.java
@@ -9,18 +9,13 @@ public class TC1 {
 		// TODO Auto-generated method stub		
 	
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\n0180400\\eclipse-workspace\\SeleniumTests\\src\\test\\java\\chromedriver.exe");		
-	
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");		
-		
-		Thread.sleep(8000);
-		
+		Thread.sleep(8000);		
 		driver.findElement(By.name("q")).sendKeys("Liberty Mutual") ;
-		Thread.sleep(4000);
-		
+		Thread.sleep(4000);		
 		driver.findElement(By.name("btnK")).click();  
 		driver.close();
-
 	}
 
 }
