7c172964dac9be61e628e24dd1d2d528792087fd
VEERAVENKATA KORLA <VEERAVENKATA.KORLA@LibertyMutual.com>
2022-01-08 20:48:46 -0800

    Commit at 8 48 PM

--- SeleniumTests/src/test/java/TC1.java
+++ SeleniumTests/src/test/java/TC1.java
@@ -2,13 +2,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TC1 {
+public class TC1
+{
 
 	public static void main(String[] args) throws InterruptedException
 	{
 		// TODO Auto-generated method stub	
-		// This comment is for testing purposes only
-	
+		// This comment is for testing purposes only	0840
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\n0180400\\eclipse-workspace\\SeleniumTests\\src\\test\\java\\chromedriver.exe");		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");		
@@ -18,5 +18,4 @@ public class TC1 {
 		driver.findElement(By.name("btnK")).click();  
 		driver.close();
 	}
-
 }
