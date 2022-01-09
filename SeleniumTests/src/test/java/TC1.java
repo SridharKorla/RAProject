bb99cb65a958e242ff7cb38192830e71fbd9b94b
VEERAVENKATA KORLA <VEERAVENKATA.KORLA@LibertyMutual.com>
2022-01-08 20:12:25 -0800

    Selenium Test cases 01/08

---
+++ SeleniumTests/src/test/java/TC1.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC1 {
+
+	public static void main(String[] args) throws InterruptedException
+	{
+		// TODO Auto-generated method stub		
+	
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\n0180400\\eclipse-workspace\\SeleniumTests\\src\\test\\java\\chromedriver.exe");		
+	
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");		
+		
+		Thread.sleep(8000);
+		
+		driver.findElement(By.name("q")).sendKeys("Liberty Mutual") ;
+		Thread.sleep(4000);
+		
+		driver.findElement(By.name("btnK")).click();  
+		driver.close();
+
+	}
+
+}
